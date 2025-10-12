5ef9c564007d9efa6cb8c4b5c9546e77c51b9b41
Justin Ward <jw4566c@gre.ac.uk>
2025-10-12 15:34:45 +0100

    player ragdolling can be enabled
    and confined head joint angular limits

--- GetBucked/Assets/Scripts/RagdollStabiliser.cs
+++ GetBucked/Assets/Scripts/RagdollStabiliser.cs
@@ -6,9 +6,17 @@ public class RagdollStabiliser : MonoBehaviour
 {
     [SerializeField] private Rigidbody rootrb;
 
-    [SerializeField] bool activateForce = true;
-    [SerializeField] float forceVal = 2f;
+    [SerializeField] bool activateForce = true, activateRagdoll = false;
+    [SerializeField] float forceVal = 2f, slerpDriveMin = 100f;
 
+    List<ConfigurableJoint> joints = new();
+    float slerpDriveVal;
+
+    private void Start()
+    {
+        AddAllJoints();
+        slerpDriveVal = joints[0].slerpDrive.positionSpring;
+    }
 
     private void FixedUpdate()
     {
@@ -16,6 +24,42 @@ public class RagdollStabiliser : MonoBehaviour
         {
             rootrb.AddForce(Vector3.up * forceVal * Time.deltaTime);
 
+        }
+        Ragdoll();
+    }
+
+    void AddAllJoints()
+    {
+        ConfigurableJoint[] allJoints = rootrb.GetComponentsInChildren<ConfigurableJoint>();
+        foreach (ConfigurableJoint j in allJoints)
+        {
+            joints.Add(j);
+        }
+    }
+
+    void Ragdoll()
+    {
+        if (activateRagdoll)
+        {
+            activateForce = false;
+            foreach (ConfigurableJoint j in joints)
+            {
+                JointDrive drive = j.slerpDrive;
+                drive.positionSpring = slerpDriveMin;
+
+                j.slerpDrive = drive;
+            }
+        }
+        else
+        {
+            
+            foreach (ConfigurableJoint j in joints)
+            {
+                JointDrive drive = j.slerpDrive;
+                drive.positionSpring = slerpDriveVal;
+
+                j.slerpDrive = drive;
+            }
         }
     }
 }
