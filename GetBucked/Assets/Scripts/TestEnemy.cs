914f560ab439640b92be764783502ed9737478d2
Justin Ward <jw4566c@gre.ac.uk>
2025-10-13 20:01:31 +0100

    added test ragdoll enemy

---
+++ GetBucked/Assets/Scripts/TestEnemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour
+{
+    Rigidbody rb;
+    ConfigurableJoint rootJoint;
+    RagdollStabiliser stabiliser;
+
+    bool ragdoll = false;
+    [SerializeField] float slerpDriveMax = 4000f, slerpDriveMin = 75f;
+    ConfigurableJoint[] bodyJoints;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rootJoint = GetComponent<ConfigurableJoint>();
+        stabiliser = GetComponentInChildren<RagdollStabiliser>();
+        bodyJoints = GetComponentsInChildren<ConfigurableJoint>();
+    }
+    private void FixedUpdate()
+    {
+        EnemyRagdoll(ragdoll);
+    }
+
+    void EnemyRagdoll(bool isRagdoll)
+    {
+        if (isRagdoll)
+        {
+            stabiliser.SetActivateForce(false);
+            SetSlerpDrive(slerpDriveMin);
+
+        }
+        else
+        {
+            stabiliser.SetActivateForce(true);
+            SetSlerpDrive(slerpDriveMax);
+
+        }
+
+    }
+    void SetSlerpDrive(float inVal)
+    {
+        foreach (ConfigurableJoint cj in bodyJoints)
+        {
+            JointDrive drive = cj.slerpDrive;
+            drive.positionSpring = inVal;
+            cj.slerpDrive = drive;
+
+        }
+
+    }
+
+    public void SetRagdoll(bool inBool)
+    {
+        ragdoll = inBool;
+    }
+}
