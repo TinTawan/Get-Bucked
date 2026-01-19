1d96bf5354a18cc50e9cad6f5068c7ae094d713d
Justin Ward <jw4566c@gre.ac.uk>
2026-01-19 16:50:07 +0000

    clamp vMouse movement
    vMouse cant move past the screen boundary

--- GetBucked/Assets/Scripts/UICursor.cs
+++ GetBucked/Assets/Scripts/UICursor.cs
@@ -1,39 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.UI;
 
 public class UICursor : MonoBehaviour
 {
-    VirtualMouseInput vMouse;
+    VirtualMouseInput virtualMouseInput;
+    Canvas canvas;
+    RectTransform canvasTransform;
 
     [SerializeField] float rayMaxDist = 5f;
     [SerializeField] LayerMask diceLayer;
 
     void Start()
     {
-        vMouse = GetComponentInParent<VirtualMouseInput>();
+        virtualMouseInput = GetComponent<VirtualMouseInput>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasTransform = canvas.GetComponent<RectTransform>();
     }
 
     void Update()
     {
-        if (vMouse.virtualMouse.leftButton.wasPressedThisFrame)
+        if (virtualMouseInput.virtualMouse.leftButton.wasPressedThisFrame)
         {
             Select();
+
         }
+    }
+
+    private void LateUpdate()
+    {
+        //stop vMouse position from going past the bounds of the screen
+        Vector2 vMousePos = virtualMouseInput.virtualMouse.position.value;
+        vMousePos.x = Mathf.Clamp(vMousePos.x, 0f, canvasTransform.rect.width);
+        vMousePos.y = Mathf.Clamp(vMousePos.y, 0f, canvasTransform.rect.height);
+        InputState.Change(virtualMouseInput.virtualMouse.position, vMousePos);
+
     }
 
     void Select()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, rayMaxDist, diceLayer))
+        if(Physics.Raycast(virtualMouseInput.cursorTransform.position, transform.forward, out hit, rayMaxDist, diceLayer))
         {
-            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green);
+            Debug.DrawRay(virtualMouseInput.cursorTransform.position, transform.forward * hit.distance, Color.green);
             Debug.Log("Hit");
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.forward * rayMaxDist, Color.red);
+            Debug.DrawRay(virtualMouseInput.cursorTransform.position, transform.forward * rayMaxDist, Color.red);
             Debug.Log("No Hit");
         }
     }
