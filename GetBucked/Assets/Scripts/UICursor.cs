ad648ad550ca98e2159e8cd60070a9a0829fe039
Justin Ward <jw4566c@gre.ac.uk>
2026-01-19 16:17:30 +0000

    check if player selects dice
    player can send raycast from vMouse to the plane and check if a dice object has been selected (debug to console)

--- GetBucked/Assets/Scripts/UICursor.cs
+++ GetBucked/Assets/Scripts/UICursor.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.UI;
 
 public class UICursor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    VirtualMouseInput vMouse;
+
+    [SerializeField] float rayMaxDist = 5f;
+    [SerializeField] LayerMask diceLayer;
+
     void Start()
     {
-        
+        vMouse = GetComponentInParent<VirtualMouseInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (vMouse.virtualMouse.leftButton.wasPressedThisFrame)
+        {
+            Select();
+        }
+    }
+
+    void Select()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayMaxDist, diceLayer))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.green);
+            Debug.Log("Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayMaxDist, Color.red);
+            Debug.Log("No Hit");
+        }
     }
 }
